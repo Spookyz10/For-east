afe508af2643cf877b1d348ccddf4eb506efa262
Spookyz10 <110704273+Spookyz10@users.noreply.github.com>
2023-05-02 01:54:15 -0300

    Update Follow.lua

--- Follow.lua
+++ Follow.lua
@@ -1,24 +1,3 @@
-_G.look = true
-while _G.look == true do
-local function Move(MobPos)
-    local character = game:GetService("Players").LocalPlayer.Character
-    local direction = (MobPos - character.Head.Position).Unit
-    character.HumanoidRootPart.CFrame = CFrame.new(character.HumanoidRootPart.Position, character.HumanoidRootPart.Position + direction)
-end
-
-local mobs = game:GetService("Workspace"):FindFirstChild("Enemies")
-
-for i,mob in pairs(mobs:GetChildren()) do
-    if mob.Name ~= "remainingEnemies" then
-        if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChildOfClass("Humanoid") then
-repeat Move(mob.HumanoidRootPart.Position) wait(0.01) until mob == nil
-        end
-    end
-end
-
-wait()
-end
-
 local OldNameCall; OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
     if getnamecallmethod() == "Kick" then return coroutine.yield() end
     return OldNameCall(self, ...)
