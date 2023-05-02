5a1f45ca771e43a98c88a9f07a67e9280c86369c
Spookyz10 <110704273+Spookyz10@users.noreply.github.com>
2023-05-02 19:39:20 -0300

    Update Follow.lua

--- Follow.lua
+++ Follow.lua
@@ -1,11 +1,26 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Spookyz10/For-east/main/Ah.lua"))()
-
 local OldNameCall; OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
     if getnamecallmethod() == "Kick" then return coroutine.yield() end
     return OldNameCall(self, ...)
 end)
 
 while wait(0.3) do
+
+local function Move(MobPos)
+    local character = game:GetService("Players").LocalPlayer.Character
+    local direction = (MobPos - character.Head.Position).Unit
+    character.HumanoidRootPart.CFrame = CFrame.new(character.HumanoidRootPart.Position, character.HumanoidRootPart.Position + direction)
+end
+
+local mobs = game:GetService("Workspace"):FindFirstChild("Enemies")
+
+for i,mob in pairs(mobs:GetChildren()) do
+    if mob.Name ~= "remainingEnemies" then
+        if mob:FindFirstChild("HumanoidRootPart") and mob:FindFirstChildOfClass("Humanoid") then
+repeat Move(mob.HumanoidRootPart.Position) wait(0.01) until mob == nil
+        end
+    end
+end
+
 game:GetService'Players'.LocalPlayer.Character:MoveTo(workspace.azvaro_gamer.HumanoidRootPart.Position)
 
 local args = {
@@ -19,5 +34,4 @@ local args = {
 }
 
 game:GetService("ReplicatedStorage"):WaitForChild("useSpell"):FireServer(unpack(args))
-wait()
 end
